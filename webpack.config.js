c8c0e4848d39503b285f6df34c6aa6fd2ff2ca4a
chrille0234 <madsen.chr@hotmail.com>
2023-10-23 08:43:44 +0200

    add files

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+}
